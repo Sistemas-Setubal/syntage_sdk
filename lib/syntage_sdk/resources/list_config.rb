a567fc0615512f62b38655074ca5e5a287987290
Mario Ezqueda <maykrayados@gmail.com>
2026-06-16 17:37:21 -0600

    docs(list_config): remove ListConfig description comment

--- lib/syntage_sdk/resources/list_config.rb
+++ lib/syntage_sdk/resources/list_config.rb
@@ -2,9 +2,6 @@
 
 module SyntageSdk
   module Resources
-    # Immutable description of how a resource lists: which option keys map to
-    # which API params for filters, numeric ranges, extra date fields, and
-    # ordering. Consumed by Listable; only `filters` is required.
     ListConfig = Data.define :filters, :numeric, :dates, :orders do
       def initialize(filters:, numeric: {}, dates: {}, orders: {})
         super
